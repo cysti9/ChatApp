c3996c2f5cac99a2e0e213d7c32f43214ee2c565
Haris Zaman <hariszamanw90@gmail.com>
2026-04-19 18:25:01 +0530

    second push

--- chat/src/main/java/com/spring/chat/ChatController.java
+++ chat/src/main/java/com/spring/chat/ChatController.java
@@ -4,17 +4,18 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 public class ChatController
 {
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
-    public String sendMessage(String message)
-    {
-        return message;
-    }
+
     public ChatMessage sendMessage(ChatMessage message)
     {
+        message.setTimestamp(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
         return message;
     }
 }
