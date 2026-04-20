549b657c9b810bda224f164f5ba2160e6d7335e6
Haris Zaman <hariszamanw90@gmail.com>
2026-04-20 18:11:48 +0530

    third commit: database and history feature

--- chat/src/main/java/com/spring/chat/ChatController.java
+++ chat/src/main/java/com/spring/chat/ChatController.java
@@ -3,19 +3,36 @@ package com.spring.chat;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Controller
 public class ChatController
 {
+    private final MessageRepo messageRepo;
+
+    public ChatController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
-
-    public ChatMessage sendMessage(ChatMessage message)
+    public ChatMessage sendMessage(ChatMessage chatmessage)
     {
-        message.setTimestamp(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
-        return message;
+        Message message = new Message(chatmessage.getSender(), chatmessage.getContent(), LocalDateTime.now());
+        messageRepo.save(message);
+        chatmessage.setTimestamp(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm")));
+        return chatmessage;
+    }
+
+    @GetMapping("/history")
+    @ResponseBody
+    public List<Message> getHistory()
+    {
+        return messageRepo.findAllByOrderByTimestampAsc();
     }
 }
