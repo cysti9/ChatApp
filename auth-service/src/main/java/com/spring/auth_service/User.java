de0182ae89cc5c03330ec514f911363c5fbbf673
Haris Zaman <hariszamanw90@gmail.com>
2026-04-23 19:48:17 +0530

    initiated microservices

---
+++ auth-service/src/main/java/com/spring/auth_service/User.java
@@ -0,0 +1,38 @@
+package com.spring.auth_service;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Users")
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    public User() {}
+
+    public User(String username,String passwordHash, String email) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.email = email;
+    }
+
+    public String getId() { return id; }
+    public String getUsername() { return username; }
+    public String getEmail() { return email; }
+    public String getPasswordHash() { return passwordHash; }
+    public void setId(String id) { this.id = id; }
+    public void setUsername(String username) { this.username = username; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+}
