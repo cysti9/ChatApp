8365db4902fe979f56f55b52583e93d648ef6ec7
Haris Zaman <hariszamanw90@gmail.com>
2026-04-24 21:55:09 +0530

    auth-service completed along with db

---
+++ auth-service/src/main/java/com/spring/auth_service/JwtFilter.java
@@ -0,0 +1,39 @@
+package com.spring.auth_service;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    public JwtFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterchain)
+            throws ServletException, IOException {
+        String authreader = request.getHeader("Authorization");
+        if (authreader != null && authreader.startsWith("Bearer ")) {
+            String token = authreader.substring(7);
+            if (jwtUtil.isTokenValid(token)) {
+                String username = jwtUtil.extractUsername(token);
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(username, null, List.of());
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterchain.doFilter(request, response);
+    }
+}
